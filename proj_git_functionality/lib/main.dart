3b36be40ea2de2fc2c3d6e5cf097794e703c664d
Victor <victorlindholmutv@gmail.com>
2023-07-06 15:30:13 +0200

    modified:   proj_git_functionality/lib/main.dart 	new file:   proj_git_functionality/lib/widgets/google_navbar.dart 	modified:   proj_git_functionality/pubspec.lock 	modified:   proj_git_functionality/pubspec.yaml

--- proj_git_functionality/lib/main.dart
+++ proj_git_functionality/lib/main.dart
@@ -1,22 +1,101 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 
 import './screens/home_screen.dart';
 import './screens/login_screen.dart';
 import './screens/profile_screen.dart';
 import './screens/settings_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
+
+void main() => runApp(MaterialApp(
+    builder: (context, child) {
+      return Directionality(textDirection: TextDirection.ltr, child: child!);
+    },
+    title: 'GNav',
+    theme: ThemeData(
+      primaryColor: Colors.grey[800],
+    ),
+    home: const MyApp()));
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    LoginScreen(),
+    SettingsScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'My App',
-      home: SettingsScreen(), // Use the HomeScreen class as the home parameter
+    return Scaffold(
+      backgroundColor: Colors.white,
+      
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.grey[300]!,
+              hoverColor: Colors.grey[100]!,
+              gap: 8,
+              activeColor: Colors.black,
+              iconSize: 24,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: const Duration(milliseconds: 400),
+              tabBackgroundColor: Colors.grey[100]!,
+              color: Colors.black,
+              tabs: const [
+                GButton(
+                  icon: LineIcons.home,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: LineIcons.heart,
+                  text: 'Likes',
+                ),
+                GButton(
+                  icon: LineIcons.search,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: LineIcons.user,
+                  text: 'Profile',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
